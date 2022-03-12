3e565dd9f7ee51122184be2cf1a004dd16e4e9bb
Nurul irfan <nurul.irfan@tokopedia.com>
2022-03-12 15:01:10 +0700

    add emotion css

--- src/newtab/components/Container.tsx
+++ src/newtab/components/Container.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import icon from '@/assets/cool-doge.png';
 
+import { sImage } from './styles';
+
 const NewTabContainer = () => {
   return (
     <main>
       <h1>Awesome Extension Tab</h1>
-      <img src={icon} alt="" />
+      <img src={icon} alt="cool doge with glasses" className={sImage} />
     </main>
   )
 }
